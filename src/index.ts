2e55aad055b32961bfe5dfa0ff79f76be6b1eef4
Will Anderson <willstoney@gmail.com>
2022-03-02 21:09:21 -0800

    test deployment

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.get("/test", (req, res) => {
-  res.status(200).send("Hello World");
+  res.status(200).send("Hello Chat Server");
 });
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
