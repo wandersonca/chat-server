49b942e2cd3796051ffd7a9417bcf7947c53c2ef
Will Anderson <willstoney@gmail.com>
2022-03-02 20:54:44 -0800

    add port

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-const PORT = 3000;
+const PORT = process.env.NODE_ENV === 'development' ? 8080 : 80;
 const app = express();
 app.get("/test", (req, res) => {
   res.status(200).send("Hello World");
