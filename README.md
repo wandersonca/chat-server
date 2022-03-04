efcd59cc8e125a3cb077fa1e21a28b61535caf0d
Will Anderson <willstoney@gmail.com>
2022-03-03 21:09:26 -0800

    changes

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # chat-server
+
+# How to run locally
+1. Install Nodejs v16
+2. Install dependancies: `npm install`
+3. Start Redis with Docker: `docker run -p 6379:6379 redis`
+4. Start the app: `npm run dev`
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,38 @@
-import express from "express";
+import express from 'express';
+import { createClient } from 'redis';
+
+
 const PORT = process.env.PORT || 3000;
 const app = express();
-app.get("/test", (req, res) => {
-  res.status(200).send("Hello Chat Server");
+
+
+async function returnRedisConnection() {
+    let client;
+    if(process.env.REDIS_URL) {
+        client = createClient(process.env.REDIS_URL);
+    } else {
+        client = createClient();
+    }
+    await client.connect();
+    return client;
+}
+
+
+app.get('/write', async (req, res) => {
+    const client = await returnRedisConnection();
+
+    await client.set('key', 'hello world');
+    res.status(200).send('Wrote Value');
 });
+
+app.get('/read', async (req, res) => {
+    const client = await returnRedisConnection();
+
+    const value = await client.get('key');
+
+    res.status(200).send(value);
+});
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
