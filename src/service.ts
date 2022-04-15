297edfa19105e5f1394112a690384e365e07d52e
wandersonca <willstoney@gmail.com>
2022-04-15 11:09:16 -0700

    testing out redis change

--- src/service.ts
+++ src/service.ts
@@ -3,21 +3,6 @@ import Account from './account.js';
 import crypto from 'crypto';
 
 export default class Service {
-    redis: any;
-    connected = false;
-
-    constructor() {
-        if(process.env.REDIS_URL) {
-            this.redis = redis.createClient({url: process.env.REDIS_URL,   socket: {
-                tls: true,
-                rejectUnauthorized: false,
-                connectTimeout: 3000,
-              }});
-        } else {
-            this.redis = redis.createClient();
-        }
-    }
-
     private validateSignature(publicKey: string, signature: string, message: string) {
         console.log(`PublicKey: ${publicKey}\nSignature: ${signature}\nMessage: ${message}`);
         const verify = crypto.createVerify('SHA256')
@@ -27,82 +12,88 @@ export default class Service {
         }
     }
 
-    async connect() {
-        try {
-            // TODO: find a cleaner way to detect if we are connected or not... 
-            await this.redis.connect();
-        } catch (error: any) {
-            if (error.message != 'Socket already opened') {
-                console.error(error)
-                throw error
-            }
+    async connect() : Promise<redis.RedisClient> {
+        let client
+        if(process.env.REDIS_URL) {
+            client = redis.createClient({url: process.env.REDIS_URL, socket: {
+                tls: true,
+                rejectUnauthorized: false,
+                connectTimeout: 3000,
+              }});
+        } else {
+            client = redis.createClient();
         }
+        await client.connect();
+        return client
     }
 
     async createAccount(newAccount: any, signature: string): Promise<Account> {
-        await this.connect();
+        const client = await this.connect();
         if(!newAccount) {
             throw new Error("Missing body");
         }
         this.validateSignature(newAccount.publicKey, signature, JSON.stringify(newAccount))
-        const id = await this.redis.incr('id-counter');
+        const id = await client.incr('id-counter');
         const createdAccount = new Account(newAccount.name, newAccount.publicKey, id);
-        await this.redis.set(id, JSON.stringify(createdAccount));
+        await client.set(id, JSON.stringify(createdAccount));
+        await client.quit();
         return createdAccount;
     }
 
     async getAccount(id: number): Promise<Account> {
-        await this.connect();
-        const account = await this.redis.get(id);
+        const client = await this.connect();
+        const account = await client.get(id);
         if(!account) {
             throw new Error("Account not found");
         } else {
             return JSON.parse(account) as Account;
         }
+        await client.quit();
     }
 
     async sendMessage(message: any, signature: string): Promise<string> {
-        await this.connect();
+        const client = await this.connect();
         if(!message) {
             throw new Error("Missing body");
         }
         const senderAccount = await this.getAccount(message.senderId);
         this.validateSignature(senderAccount.publicKey, signature, JSON.stringify(message))
         const recipient = message.recipientId; 
-        if(!await this.redis.exists(recipient)) {
+        if(!await client.exists(recipient)) {
             throw new Error("Recipient does not exist");
         }
-        const counter = await this.redis.incr(`${recipient}:count`);
+        const counter = await client.incr(`${recipient}:count`);
         message.messageId = counter;
         const messageString = JSON.stringify(message)
         if (message.ttl) {
             console.log(`${recipient}:message:${counter}  ${messageString} EX ${message.ttl}`);
-            await this.redis.set(`${recipient}:message:${counter}`, messageString, {'EX': message.ttl});
+            await client.set(`${recipient}:message:${counter}`, messageString, {'EX': message.ttl});
         } else {
             console.log(`${recipient}:message:${counter}  ${messageString}`);
-            await this.redis.set(`${recipient}:message:${counter}`, messageString);            
+            await client.set(`${recipient}:message:${counter}`, messageString);            
         }
-
+        await client.quit();
         return messageString
     }
 
     async getMessages(id: number, signature: string): Promise<Array<string>> {
-        await this.connect();
+        const client = await this.connect();;
         const senderAccount = await this.getAccount(id);
         this.validateSignature(senderAccount.publicKey, signature, `${id}`);
 
         const messages: Array<string> = [];
-        if(!await this.redis.exists(id)) {
+        if(!await client.exists(id)) {
             throw new Error("Recipient does not exist");
         }
         console.log(`${id}:message-*`);
-        const scanIterator = this.redis.scanIterator({TYPE: 'string', MATCH: `${id}:message:*`});
+        const scanIterator = client.scanIterator({TYPE: 'string', MATCH: `${id}:message:*`});
         for await (const key of scanIterator) {
-            let message = await this.redis.getDel(key);
+            let message = await client.getDel(key);
             message = JSON.parse(message);
             console.log(`Key: ${key} Message: ${message}`);
             messages.push(message);
         }
+        await client.quit();
         return messages;
     }
 }
